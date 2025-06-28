877cd72fe378574d110e036bec7542fc77a716d9
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-06-29 03:26:31 +0700

    fixing some backend auth and frontend ui

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,30 +5,39 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 const secretKey = 'your_secret_key';
 
-let users = [];
+const User = require('../models/User');
 
 // Signup route
-router.post('/signup', async (req, res) => {
+router.post('/auth/signup', async (req, res) => {
   const { username, password } = req.body;
+
+  // Cek apakah username sudah ada
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return res.status(400).send('Username already taken');
+  }
+
   // Hash password
   const hashedPassword = await bcrypt.hash(password, 8);
-  // Store user
-  users.push({ username, password: hashedPassword });
+
+  // Simpan user ke database
+  const newUser = new User({ username, password: hashedPassword });
+  await newUser.save();
+
   res.status(201).send('User created');
 });
 
 // Login route
-router.post('/login', async (req, res) => {
+router.post('/auth/login', async (req, res) => {
   const { username, password } = req.body;
-  const user = users.find(u => u.username === username);
 
+  const user = await User.findOne({ username });
   if (!user || !(await bcrypt.compare(password, user.password))) {
     return res.status(401).send('Invalid credentials');
   }
 
-  // Generate token
   const token = jwt.sign({ userId: user.username }, secretKey, { expiresIn: '1h' });
-  res.cookie('authcookie',token,{maxAge:900000,httpOnly:true}) 
+  res.cookie('authcookie', token, { maxAge: 900000, httpOnly: true });
 
   res.status(200).send({ token });
 });
--- frontend/src/app/signup/mainbody.jsx
+++ frontend/src/app/signup/mainbody.jsx
@@ -1,11 +1,27 @@
+'use client';
+import { useState } from "react"
+
 export default function MainBody(){
+  
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return(
     <>
       <div className="bg-[#45475a] p-3 w-1/4">
         <p className="text-white">Sign Up</p>
         <div className="flex flex-col">
-          <input placeholder="Email" className="bg-[#b4befe] p-1 my-1"/>
-          <input placeholder="Password" className="bg-[#b4befe] p-1"/>
+          <input placeholder="Email" className="bg-[#b4befe] p-1 my-1" value={email} onChange={handleChangeEmail}/>
+          <input placeholder="Password" className="bg-[#b4befe] p-1" value={password} onChange={handleChangePassword}/>
         </div>
         <div className="flex flex-col">
           <button className="bg-[#f38ba8] mt-2 mb-1">Sign Up</button>

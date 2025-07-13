a747367d72bec417daf46ec92caba1fdace85dee
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-07-13 22:24:05 +0700

    just cloning from js

---
+++ frontend-typescript/src/app/generatecardsection.tsx
@@ -0,0 +1,25 @@
+import Cardsection from "./cardsection"
+
+export default function Generatecardsection() {
+  return(
+    <>
+      <div className="flex flex-wrap gap-10 justify-between">
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+        <Cardsection/>
+      </div>
+    </>
+  )
+}

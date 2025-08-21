cb0814070f0ef0bd37cc0e400cacecb9c70c6b40
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-08-21 18:11:42 +0700

    adding some controller and making dir for backend rewrite to typescript

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,54 +1,17 @@
 import express from "express";
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 const router = express.Router();
 const secretKey = process.env.SECRET_KEY;
 
-import User from "../models/userModel.js";
+import * as authController from '../controllers/authController.js'
 
 // Signup route
-router.post("/signup", async (req, res) => {
-  const { email, password } = req.body;
-
-  // Cek apakah username sudah ada
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return res.status(400).send("Username already taken");
-  }
-
-  // Hash password
-  const hashedPassword = await bcrypt.hash(password, 8);
-
-  // Simpan user ke database
-  const newUser = new User({ email, password: hashedPassword });
-  await newUser.save();
-
-  res.status(201).send("User created");
-});
+router.post("/signup", authController.signUp)
 
 // Login route
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email });
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(401).send("Invalid credentials");
-  }
-
-  const token = jwt.sign({ userId: user.email }, secretKey, {
-    expiresIn: "1h",
-  });
-  res.cookie("authcookie", token, {
-    maxAge: 900000,
-    httpOnly: true,
-    secure: false,
-    sameSite: "Lax",
-  });
-
-  res.status(200).send({ token });
-});
+router.post("/login", authController.login)
 
 router.get("/verify", (req, res) => {
   const token = req.cookies.authcookie;

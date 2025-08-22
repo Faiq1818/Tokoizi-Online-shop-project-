ceab22acc0d9f3bf39fcf0f931995560c9b3fbdf
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-08-22 19:01:40 +0700

    add src/config and mongodb config file and making env.d.ts

---
+++ backend-typescript-rewrite/src/config/dbConfig.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const uri = process.env.MONGO_URI_DEV;
+
+async function connectDB() {
+  try {
+    mongoose.connect(uri, {
+      dbName: "TokoiziDB",
+    });
+    console.log("Connection to Mongodb Successful");
+  } catch (err) {
+    console.error("❌ MongoDB Connection Error:", err);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
--- backend-typescript-rewrite/src/index.ts
+++ backend-typescript-rewrite/src/index.ts
@@ -1,3 +1,21 @@
+// Dependency
 import express from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
+// Config
+import configDB from "./config/dbConfig.js";
+const app = express();
+const port = 3000;
+
+
+
+// Database Connect
+configDB();
+
+// Checking port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
